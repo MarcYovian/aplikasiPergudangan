7848647daf8c3e139e36e100fcd674ba27ae5c9c
Marcellinus Yovian <marcellinusyovian@gmail.com>
2023-01-04 16:30:51 +0700

    kasaran dasboard

---
+++ AplikasiPergudangan/src/com/component/menu.java
@@ -0,0 +1,41 @@
+
+package com.component;
+
+import java.awt.Graphics;
+import java.awt.*;
+
+public class menu extends javax.swing.JPanel {
+
+    public menu() {
+        initComponents();
+        setOpaque(false);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 215, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 396, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+    @Override
+    protected void paintChildren(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        GradientPaint gp = new GradientPaint(0,0,Color.decode("#ff9966"), 0, getHeight(),Color.decode("#ff5e62"));
+        g2.setPaint(gp);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        super.paintChildren(g);
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
