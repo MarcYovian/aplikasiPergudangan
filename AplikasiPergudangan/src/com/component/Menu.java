f8a7c34ce2d84c3b1cc4458aa3bb944a0c303379
Fender <Fender@Nder>
2023-01-08 22:24:24 +0700

    - menu rename to Menu - MyTextField for Text Field Template - MyPasswordField Text Field for Passwords - MyButton for Button Template - Login page Updated - 3 icon (mail,user,pass) added

---
+++ AplikasiPergudangan/src/com/component/Menu.java
@@ -0,0 +1,122 @@
+
+package com.component;
+
+import com.event.EventMenuSelected;
+import com.model.model_Menu;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Menu extends javax.swing.JPanel {
+
+    private EventMenuSelected event;
+    
+    public void addEventMenuSelected(EventMenuSelected event) {
+        this.event = event;
+        listMenu1.addEventMenuSelected(event);
+    }
+    
+    public Menu() {
+        initComponents();
+        setOpaque(false);
+        listMenu1.setOpaque(false);
+        init();
+    }
+    
+    private void init() {
+        listMenu1.addItem(new model_Menu("home", "Dashboard", model_Menu.MenuType.MENU));
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelMoving = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        listMenu1 = new com.swing.listMenu<>();
+
+        panelMoving.setOpaque(false);
+
+        jLabel1.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/icon/warehouse.png"))); // NOI18N
+        jLabel1.setText("Warehouse Application");
+
+        javax.swing.GroupLayout panelMovingLayout = new javax.swing.GroupLayout(panelMoving);
+        panelMoving.setLayout(panelMovingLayout);
+        panelMovingLayout.setHorizontalGroup(
+            panelMovingLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 247, Short.MAX_VALUE)
+        );
+        panelMovingLayout.setVerticalGroup(
+            panelMovingLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelMovingLayout.createSequentialGroup()
+                .addContainerGap(15, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        listMenu1.setOpaque(false);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelMoving, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(listMenu1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(panelMoving, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(listMenu1, javax.swing.GroupLayout.DEFAULT_SIZE, 426, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+    @Override
+    protected void paintChildren(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        GradientPaint gp = new GradientPaint(0,0,Color.decode("#ff9966"), 0, getHeight(),Color.decode("#ff5e62"));
+        g2.setPaint(gp);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 15);
+        g2.fillRect(getWidth()-20, 0, getWidth(), getHeight());
+        super.paintChildren(g);
+    }
+    private int x;
+    private int y;
+
+    public void initMoving(JFrame frame) {
+        panelMoving.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                x = me.getX();
+                y = me.getY();
+            }
+
+        });
+        panelMoving.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent me) {
+                frame.setLocation(me.getXOnScreen() - x, me.getYOnScreen() - y);
+            }
+        });
+    }
+    
+    
+    
+//    public void initMoving(JFrame frame){
+//        panelMoving.addMouseListener(new MouseAdapter(){
+//            @Override
+//            public void mousePressed(MouseEvent me){
+//                super.mousePressed(me);
+//            }
+//        });
+//    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private com.swing.listMenu<String> listMenu1;
+    private javax.swing.JPanel panelMoving;
+    // End of variables declaration//GEN-END:variables
+}
